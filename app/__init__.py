04100a4e1aee06e0a83cf40564bedf3a9e592c6c
LingxiaoMa <malingxiao930@gmail.com>
2024-09-26 11:37:25 +0800

    Some changes to init and run

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-def create_app():
-    app = Flask(__name__)
 
-    # 注册路由
-    from .routes import main
-    app.register_blueprint(main)
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-    return app
+
+from app import routes, models
+    
+
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import app, db
 
-app = create_app()
+
+# 注册路由
+from app.routes import main
+app.register_blueprint(main)
 
 if __name__ == "__main__":
     app.run(debug=True)
